cb2ceb74fd8fedfe22761dc3aee8091b574a2aef
boudewijnbout <boudewijn_bout@outlook.com>
2022-03-29 01:27:15 +0200

    Add DK feedback

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const port = 3000;
 // Imports
 import express from "express";
 import fetch from "node-fetch";
-import ejs from "ejs";
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+// Overview page
 app.get("/", (req, res) => {
   fetch("https://chipr.api.fdnd.nl/v1/project")
     .then((res) => res.json())
     .then((data) => {
-      res.render("index", { data: data });
+      res.render("index", { data: data.data });
     });
 });
 
