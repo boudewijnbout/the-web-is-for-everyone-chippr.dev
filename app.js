96e7bc660def7328080aef468dcf9697684dc02d
boudewijnbout <boudewijn_bout@outlook.com>
2022-03-29 12:39:06 +0200

    Added overview page v0.1

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const port = 3000;
 import express from "express";
 import fetch from "node-fetch";
 
+// Uses
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 // Overview page
 app.get("/", (req, res) => {
+
+  // Fetch all projects and send them to the overview page
   fetch("https://chipr.api.fdnd.nl/v1/project")
     .then((res) => res.json())
     .then((data) => {
